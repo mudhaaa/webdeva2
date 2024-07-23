861f13d5c7fc164e8b1cbe1f3cf9fbe651cfaeb9
mudhaaa <129643813+mudhaaa@users.noreply.github.com>
2024-07-24 02:18:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ const page4mainbtn = document.querySelector("#mainMenuBtn4");
 //target pages
 const indexPage = document.querySelector("#indexContainer");
 const leaguePage = document.querySelector("#leagueContainer");
-const penaltyPage = document.querySelector("#penaltyContainer");
+const gamePage = document.querySelector("#gameContainer");
 
 //hide all pages funtion
 function hideAll() {
   indexPage.classList.remove("show");
   leaguePage.classList.remove("show");
-  penaltyPage.classList.remove("show");
+  gamePage.classList.remove("show");
 }
 
 //function to show selected page no
@@ -34,12 +34,12 @@ function show(pageNo) {
   //show the page
   if (pageNo == 1) indexPage.classList.add("show");
   if (pageNo == 2) leaguePage.classList.add("show");
-  if (pageNo == 4) penaltyPage.classList.add("show");
+  if (pageNo == 4) gamePage.classList.add("show");
 }
 
 //hide all other
 hideAll();
-show(2);
+show(4);
 
 /*Listen for clicks on the buttons, assign anonymous
 eventhandler functions to call show function*/
@@ -133,59 +133,236 @@ document.getElementById("rightButton").addEventListener("click", function () {
   showSlides();
 });
 
-//penalty game
+//goalkeeper game
+//board
+let board;
+let boardWidth = 500;
+let boardHeight = 500;
+let context; 
 
-//target game elements
-const ball = document.getElementById("ball");
-const gk = document.getElementById("goalkeeper");
-const shootBtn = document.getElementById("shootButton");
-const resetBtn = document.getElementById("resetButton");
+//players
+let playerWidth = 80; //500 for testing, 80 normal
+let playerHeight = 10;
+let playerVelocityX = 10; //move 10 pixels each time
 
-//function that resets ball
-function resetBall() {
-  ball.style.right = "1150px";
+let player = {
+    x : boardWidth/2 - playerWidth/2,
+    y : boardHeight - playerHeight - 5,
+    width: playerWidth,
+    height: playerHeight,
+    velocityX : playerVelocityX
 }
 
-//function to update ball css as well as display text
-function moveBall(ballX, ballY) {
-  ball.style.right = ballX + "%"; //set right property to ball x variable
-  ball.style.top = ballY + "px"; //set top property to ball x variable
+//ball
+let ballWidth = 10;
+let ballHeight = 10;
+let ballVelocityX = 3; //15 for testing, 3 normal
+let ballVelocityY = 2; //10 for testing, 2 normal
+
+let ball = {
+    x : boardWidth/2,
+    y : boardHeight/2,
+    width: ballWidth,
+    height: ballHeight,
+    velocityX : ballVelocityX,
+    velocityY : ballVelocityY
 }
 
-//to move goalkeeper
-function moveGK(randY) {
-  gk.style.top = "850px";
-  gk.style.right = randY + "%";
+//blocks
+let blockArray = [];
+let blockWidth = 50;
+let blockHeight = 10;
+let blockColumns = 8; 
+let blockRows = 3; //add more as game goesd  on
+let blockMaxRows = 10; //limit how many rows
+let blockCount = 0;
+
+//starting block corners top left 
+let blockX = 15;
+let blockY = 45;
+
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    board = document.getElementById("game");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //draw initial player
+    context.fillStyle="skyblue";
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer);
+
+    //create blocks
+    createBlocks();
 }
 
-//to shoot ball
-function shootBall() {
-  let randY = Math.floor(Math.random * 60) + 20; //range of 20 - 60
-  moveBall(currX, 850); //moves ball forward at the
-  moveGK(randY);
-  console.log("shoot!");
+function update() {
+    requestAnimationFrame(update);
+    //stop drawing
+    if (gameOver) {
+        return;
+    }
+    context.clearRect(0, 0, board.width, board.height);
+
+    // player
+    context.fillStyle = "red";
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    // ball
+    context.fillStyle = "white";
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    //bounce the ball off player paddle
+    if (topCollision(ball, player) || bottomCollision(ball, player)) {
+        ball.velocityY *= -1;   // flip y direction up or down
+    }
+    else if (leftCollision(ball, player) || rightCollision(ball, player)) {
+        ball.velocityX *= -1;   // flip x direction left or right
+    }
+
+    if (ball.y <= 0) { 
+        // if ball touches top of canvas
+        ball.velocityY *= -1; //reverse direction
+    }
+    else if (ball.x <= 0 || (ball.x + ball.width >= boardWidth)) {
+        // if ball touches left or right of canvas
+        ball.velocityX *= -1; //reverse direction
+    }
+    else if (ball.y + ball.height >= boardHeight) {
+        // if ball touches bottom of canvas
+        context.font = "20px sans-serif";
+        context.fillText("Game Over: Press 'Space' to Restart", 80, 400);
+        gameOver = true;
+    }
+
+    //blocks
+    context.fillStyle = "skyblue";
+    for (let i = 0; i < blockArray.length; i++) {
+        let block = blockArray[i];
+        if (!block.break) {
+            if (topCollision(ball, block) || bottomCollision(ball, block)) {
+                block.break = true;     // block is broken
+                ball.velocityY *= -1;   // flip y direction up or down
+                score += 100;
+                blockCount -= 1;
+            }
+            else if (leftCollision(ball, block) || rightCollision(ball, block)) {
+                block.break = true;     // block is broken
+                ball.velocityX *= -1;   // flip x direction left or right
+                score += 100;
+                blockCount -= 1;
+            }
+            context.fillRect(block.x, block.y, block.width, block.height);
+        }
+    }
+
+    //next level
+    if (blockCount == 0) {
+        score += 100*blockRows*blockColumns; //bonus points :)
+        blockRows = Math.min(blockRows + 1, blockMaxRows);
+        createBlocks();
+    }
+
+    //score
+    context.font = "20px sans-serif";
+    context.fillText(score, 10, 25);
 }
 
-//init ball moving var and bool
-var moveBallX = 0;
-var movingRight = true;
-var currX;
-
-//repeatedly call all the functions
-function gameLoop() {
-  moveBall(moveBallX, 1150);
-  if (moveBallX == 10 && movingRight == true) {
-    movingRight = false;
-    moveBallX = moveBallX + 1;
-  } else if (moveBallX == 80 && movingRight == false) {
-    movingRight = true;
-    moveBallX = moveBallX - 1;
-  }
-  currX = moveBallX;
+function outOfBounds(xPosition) {
+    return (xPosition < 0 || xPosition + playerWidth > boardWidth);
 }
 
-//event listener for shoot and reset button
-shootBtn.addEventListener("click", shootBall());
-resetBtn.addEventListener("click", resetBall());
+function movePlayer(e) {
+    if (gameOver) {
+        if (e.code == "Space") {
+            resetGame();
+            console.log("RESET");
+        }
+        return;
+    }
+    if (e.code == "ArrowLeft") {
+        // player.x -= player.velocityX;
+        let nextplayerX = player.x - player.velocityX;
+        if (!outOfBounds(nextplayerX)) {
+            player.x = nextplayerX;
+        }
+    }
+    else if (e.code == "ArrowRight") {
+        let nextplayerX = player.x + player.velocityX;
+        if (!outOfBounds(nextplayerX)) {
+            player.x = nextplayerX;
+        }
+        // player.x += player.velocityX;    
+    }
+}
 
-gameLoop();
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
+           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
+           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+}
+
+function topCollision(ball, block) { //a is above b (ball is above block)
+    return detectCollision(ball, block) && (ball.y + ball.height) >= block.y;
+}
+
+function bottomCollision(ball, block) { //a is above b (ball is below block)
+    return detectCollision(ball, block) && (block.y + block.height) >= ball.y;
+}
+
+function leftCollision(ball, block) { //a is left of b (ball is left of block)
+    return detectCollision(ball, block) && (ball.x + ball.width) >= block.x;
+}
+
+function rightCollision(ball, block) { //a is right of b (ball is right of block)
+    return detectCollision(ball, block) && (block.x + block.width) >= ball.x;
+}
+
+function createBlocks() {
+    blockArray = []; //clear blockArray
+    for (let c = 0; c < blockColumns; c++) {
+        for (let r = 0; r < blockRows; r++) {
+            let block = {
+                x : blockX + c*blockWidth + c*10, //c*10 space 10 pixels apart columns
+                y : blockY + r*blockHeight + r*10, //r*10 space 10 pixels apart rows
+                width : blockWidth,
+                height : blockHeight,
+                break : false
+            }
+            blockArray.push(block);
+        }
+    }
+    blockCount = blockArray.length;
+}
+
+function resetGame() {
+    gameOver = false;
+    player = {
+        x : boardWidth/2 - playerWidth/2,
+        y : boardHeight - playerHeight - 5,
+        width: playerWidth,
+        height: playerHeight,
+        velocityX : playerVelocityX
+    }
+    ball = {
+        x : boardWidth/2,
+        y : boardHeight/2,
+        width: ballWidth,
+        height: ballHeight,
+        velocityX : ballVelocityX,
+        velocityY : ballVelocityY
+    }
+    blockArray = [];
+    blockRows = 3;
+    score = 0;
+    createBlocks();
+}
+
